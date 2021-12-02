0c03a52d0057a1c7c6d035dc1ac34652b3b0d1fe
hasnat23 <chmuhammadhasnat23@gmail.com>
2021-12-02 16:49:46 +0100

    setting up the cards for products listing

--- components/Card.js
+++ components/Card.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Dimensions, Image, TouchableOpacity, Text, View} from 'react-native';
+import { Dimensions, Image, TouchableOpacity, Text, View } from 'react-native';
 import { sWidth } from '../constants/dimensions';
 import capitalizeName from '../utility/capitalize';
 
-const Card = ({data}) => {
+const Card = ({ data }) => {
   return (
     <TouchableOpacity
       activeOpacity={0.9}
@@ -13,37 +13,40 @@ const Card = ({data}) => {
         flexDirection: 'row',
         height: 120,
         flex: 1,
-        borderRadius: 10,
+        borderRadius: 9,
         padding: 5,
       }}>
       <Image
-        style={{flex: 1, borderRadius: 10}}
+        style={{ flex: 1, borderRadius: 10 }}
         source={{
           uri: 'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
         }}
       />
 
-      <View style={{padding: 6, justifyContent: 'space-between', flex: 3}}>
-        <Text style={{color: '#5280e2'}}>{capitalizeName(data.brandname)}</Text>
-        <Text style={{fontWeight: '700', fontSize: 16}}>{data.name.toUpperCase()}</Text>
-        <Text numberOfLines={1} style={{fontSize: 14, color: '#bdbcbc'}}>{data.description}</Text>
+      <View style={{ padding: 6, justifyContent: 'space-between', flex: 3 }}>
+        <Text style={{ color: '#5280e2' }}>{capitalizeName(data.brandname)}</Text>
+        <Text style={{ fontWeight: '700', fontSize: 16 }}>{data.name.toUpperCase()}</Text>
+        <Text numberOfLines={1} style={{ fontSize: 14, color: '#bdbcbc' }}>{data.description}</Text>
         <View
           style={{
             justifyContent: 'space-between',
-            alignItems:'center',
+            alignItems: 'center',
             flex: 1,
             flexDirection: 'row',
           }}>
-          <View style={{backgroundColor: '#3dd5a2', height:20, paddingLeft:8, paddingRight:8, borderRadius:sWidth/7, alignItems:'center', justifyContent:'center'}}>
-            <Text style={{color:'#FFFFFF'}}>New</Text>
+          <View style={{ backgroundColor: '#3dd5a2', height: 20, paddingLeft: 8, paddingRight: 8, borderRadius: sWidth / 7, alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={{ color: '#FFFFFF' }}>New</Text>
           </View>
-          <View style={{paddingRight:6}}>
+          <View style={{ paddingRight: 6 }}>
             <Text>${data.price}</Text>
           </View>
         </View>
       </View>
     </TouchableOpacity>
   );
+
+
 };
+
 
 module.exports = Card;
